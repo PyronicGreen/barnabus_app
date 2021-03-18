edc3415b4fe90e8f21c11f96234b769167f45c80
PyronicGreen <63386020+PyronicGreen@users.noreply.github.com>
2021-03-18 00:30:05 -0400

    Add files via upload
    The masterpiece thus far. Modal for "account" added but not 100% functional yet. So far just landing page, mapbox map, a little bit of  CSS animation and some very unfinished JS.

---
+++ script.js
@@ -0,0 +1,57 @@
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+mapboxgl.accessToken = 
+'pk.eyJ1IjoicGlsZ3JpbW5vaXNlIiwiYSI6ImNrbTFjdHN1NzAwZjQycm16OWk5MGQzN24ifQ.0pokCFyKGSVxD3HTtk4SNg'
+
+navigator.geolocation.getCurrentPosition(successLocation, 
+errorLocation, {enableHighAccuracy: true})
+
+function successLocation(lat, long) {
+    setupMap([-73.95, 40.825])
+}
+
+function errorLocation() {
+    setupMap([-100, 30.26])
+}
+
+function setupMap(center) {
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/pilgrimnoise/ckm57ui277mzi17r1uux34q8y',
+        center: center,
+        zoom: 13.5
+    })
+
+    const nav = new mapboxgl.NavigationControl()
+    map.addControl(nav)
+}
+
+map.on('click', function(e) {
+    var features = map.queryRenderedFeatures(e.point, {
+      layers: ['event-spaces']
+    });
+  
+    if (!features.length) {
+      return;
+    }
+  
+    var feature = features[0];
+  
+    var popup = new mapboxgl.Popup({ offset: [0, -15] })
+      .setLngLat(feature.geometry.coordinates)
+      .setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p>')
+      .addTo(map);
+  });
+
+  // Show modal
+  open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+  // Hide modal
+  close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+  // Hide modal on outside click
+  window.addEventListener('click', e => e.target == 
+    modal ? modal.classList.remove('show-modal') : false );
